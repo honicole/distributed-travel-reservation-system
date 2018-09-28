f8464de1c3cd26b6f78bafa1c43326c783a3c50a
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2018-09-27 20:38:46 -0400

    Reformatted files, and replaced deprecated code

--- Client/Client/RMIClient.java
+++ Client/Client/RMIClient.java
@@ -10,83 +10,73 @@ import java.rmi.NotBoundException;
 import java.util.*;
 import java.io.*;
 
-public class RMIClient extends Client
-{
-	private static String s_serverHost = "localhost";
-	private static int s_serverPort = 1099;
-	private static String s_serverName = "Server";
-	private static String s_rmiPrefix = "group3_";
+public class RMIClient extends Client {
+  private static String s_serverHost = "localhost";
+  private static int s_serverPort = 1099;
+  private static String s_serverName = "Server";
+  private static String s_rmiPrefix = "group3_";
 
-	public static void main(String args[])
-	{	
-		if (args.length > 0)
-		{
-			s_serverHost = args[0];
-		}
-		if (args.length > 1)
-		{
-			s_serverName = args[1];
-		}
-		if (args.length > 2)
-		{
-			System.err.println((char)27 + "[31;1mClient exception: " + (char)27 + "[0mUsage: java client.RMIClient [server_hostname [server_rmiobject]]");
-			System.exit(1);
-		}
+  public static void main(String args[]) {
+    if (args.length > 0) {
+      s_serverHost = args[0];
+    }
+    if (args.length > 1) {
+      s_serverName = args[1];
+    }
+    if (args.length > 2) {
+      System.err.println((char) 27 + "[31;1mClient exception: " + (char) 27
+          + "[0mUsage: java client.RMIClient [server_hostname [server_rmiobject]]");
+      System.exit(1);
+    }
 
-		// Set the security policy
-		if (System.getSecurityManager() == null)
-		{
-			System.setSecurityManager(new SecurityManager());
-		}
+    // Set the security policy
+    if (System.getSecurityManager() == null) {
+      System.setSecurityManager(new SecurityManager());
+    }
 
-		// Get a reference to the RMIRegister
-		try {
-			RMIClient client = new RMIClient();
-			client.connectServer();
-			client.start();
-		} 
-		catch (Exception e) {    
-			System.err.println((char)27 + "[31;1mClient exception: " + (char)27 + "[0mUncaught exception");
-			e.printStackTrace();
-			System.exit(1);
-		}
-	}
+    // Get a reference to the RMIRegister
+    try {
+      RMIClient client = new RMIClient();
+      client.connectServer();
+      client.start();
+    } catch (Exception e) {
+      System.err.println((char) 27 + "[31;1mClient exception: " + (char) 27 + "[0mUncaught exception");
+      e.printStackTrace();
+      System.exit(1);
+    }
+  }
 
-	public RMIClient()
-	{
-		super();
-	}
+  public RMIClient() {
+    super();
+  }
 
-	public void connectServer()
-	{
-		connectServer(s_serverHost, s_serverPort, s_serverName);
-	}
+  public void connectServer() {
+    connectServer(s_serverHost, s_serverPort, s_serverName);
+  }
 
-	public void connectServer(String server, int port, String name)
-	{
-		try {
-			boolean first = true;
-			while (true) {
-				try {
-					Registry registry = LocateRegistry.getRegistry(server, port);
-					m_resourceManager = (IResourceManager)registry.lookup(s_rmiPrefix + name);
-					System.out.println("Connected to '" + name + "' server [" + server + ":" + port + "/" + s_rmiPrefix + name + "]");
-					break;
-				}
-				catch (NotBoundException|RemoteException e) {
-					if (first) {
-						System.out.println("Waiting for '" + name + "' server [" + server + ":" + port + "/" + s_rmiPrefix + name + "]");
-						first = false;
-					}
-				}
-				Thread.sleep(500);
-			}
-		}
-		catch (Exception e) {
-			System.err.println((char)27 + "[31;1mServer exception: " + (char)27 + "[0mUncaught exception");
-			e.printStackTrace();
-			System.exit(1);
-		}
-	}
+  public void connectServer(String server, int port, String name) {
+    try {
+      boolean first = true;
+      while (true) {
+        try {
+          Registry registry = LocateRegistry.getRegistry(server, port);
+          m_resourceManager = (IResourceManager) registry.lookup(s_rmiPrefix + name);
+          System.out.println("Connected to '" + name 
+              + "' server [" + server + ":" + port + "/" + s_rmiPrefix + name + "]");
+          break;
+        } catch (NotBoundException | RemoteException e) {
+          if (first) {
+            System.out.println("Waiting for '" + name 
+                + "' server [" + server + ":" + port + "/" + s_rmiPrefix + name + "]");
+            first = false;
+          }
+        }
+        Thread.sleep(500);
+      }
+    } catch (Exception e) {
+      System.err.println((char) 27 + "[31;1mServer exception: " + (char) 27 + "[0mUncaught exception");
+      e.printStackTrace();
+      System.exit(1);
+    }
+  }
 }
-
