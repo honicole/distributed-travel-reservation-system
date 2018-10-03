ebc2bf8abd0ae2eb2fa3a0c95d7735cf30efc346
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2018-10-02 20:58:49 -0400

    Call F,C,R servers from middleware

--- Middleware/middleware/Middleware.java
+++ Middleware/middleware/Middleware.java
@@ -1,143 +1,138 @@
 package middleware;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.rmi.ConnectException;
 import java.rmi.RemoteException;
+import java.rmi.ServerException;
+import java.rmi.UnmarshalException;
+import java.util.StringTokenizer;
 import java.util.Vector;
 
+import Client.Command;
 import Server.Interface.IResourceManager;
 
 public class Middleware implements IResourceManager {
   
   IResourceManager[] resourceManagers;
   
+  IResourceManager flightManager;
+  IResourceManager carManager;
+  IResourceManager roomManager;
+  
   public Middleware(IResourceManager... resourceManagers) {
     this.resourceManagers = resourceManagers;
+    
+    flightManager = resourceManagers[0];
+    carManager = resourceManagers[1];
+    roomManager = resourceManagers[2];
   }
   
-
+  
   @Override
   public boolean addFlight(int id, int flightNum, int flightSeats, int flightPrice) throws RemoteException {
-    // for testing purposes only
-    return true; // return FRM.addFlight(same params)
+    return flightManager.addFlight(id, flightNum, flightSeats, flightPrice);
   }
 
   @Override
   public boolean addCars(int id, String location, int numCars, int price) throws RemoteException {
-    // TODO Auto-generated method stub
-    return false;
+    return carManager.addCars(id, location, numCars, price);
   }
 
   @Override
   public boolean addRooms(int id, String location, int numRooms, int price) throws RemoteException {
-    // TODO Auto-generated method stub
-    return false;
+    return roomManager.addRooms(id, location, numRooms, price);
   }
 
   @Override
   public int newCustomer(int id) throws RemoteException {
-    // TODO Auto-generated method stub
     return 0;
   }
 
   @Override
   public boolean newCustomer(int id, int cid) throws RemoteException {
-    // TODO Auto-generated method stub
     return false;
   }
 
   @Override
   public boolean deleteFlight(int id, int flightNum) throws RemoteException {
-    // TODO Auto-generated method stub
-    return false;
+    return flightManager.deleteFlight(id, flightNum);
   }
 
   @Override
   public boolean deleteCars(int id, String location) throws RemoteException {
-    // TODO Auto-generated method stub
-    return false;
+    return carManager.deleteCars(id, location);
   }
 
   @Override
   public boolean deleteRooms(int id, String location) throws RemoteException {
-    // TODO Auto-generated method stub
-    return false;
+    return roomManager.deleteRooms(id, location);
   }
 
   @Override
   public boolean deleteCustomer(int id, int customerID) throws RemoteException {
-    // TODO Auto-generated method stub
     return false;
   }
 
   @Override
   public int queryFlight(int id, int flightNumber) throws RemoteException {
-    // TODO Auto-generated method stub
-    return 0;
+    return flightManager.queryFlight(id, flightNumber);
   }
 
   @Override
   public int queryCars(int id, String location) throws RemoteException {
-    // TODO Auto-generated method stub
-    return 0;
+    return carManager.queryCars(id, location);
   }
 
   @Override
   public int queryRooms(int id, String location) throws RemoteException {
-    // TODO Auto-generated method stub
-    return 0;
+    return roomManager.queryRooms(id, location);
   }
 
   @Override
   public String queryCustomerInfo(int id, int customerID) throws RemoteException {
-    // TODO Auto-generated method stub
     return null;
   }
 
   @Override
   public int queryFlightPrice(int id, int flightNumber) throws RemoteException {
-    // TODO Auto-generated method stub
-    return 0;
+    return flightManager.queryFlightPrice(id, flightNumber);
   }
 
   @Override
   public int queryCarsPrice(int id, String location) throws RemoteException {
-    // TODO Auto-generated method stub
-    return 0;
+    return flightManager.queryCarsPrice(id, location);
   }
 
   @Override
   public int queryRoomsPrice(int id, String location) throws RemoteException {
-    // TODO Auto-generated method stub
-    return 0;
+    return roomManager.queryRoomsPrice(id, location);
   }
 
   @Override
   public boolean reserveFlight(int id, int customerID, int flightNumber) throws RemoteException {
-    // TODO Auto-generated method stub
-    return false;
+    return flightManager.reserveFlight(id, customerID, flightNumber);
   }
 
   @Override
   public boolean reserveCar(int id, int customerID, String location) throws RemoteException {
-    // TODO Auto-generated method stub
-    return false;
+    return carManager.reserveCar(id, customerID, location);
   }
 
   @Override
   public boolean reserveRoom(int id, int customerID, String location) throws RemoteException {
-    // TODO Auto-generated method stub
-    return false;
+    return roomManager.reserveRoom(id, customerID, location);
   }
 
   @Override
   public boolean bundle(int id, int customerID, Vector<String> flightNumbers, String location, boolean car,
       boolean room) throws RemoteException {
-    // TODO Auto-generated method stub
     return false;
   }
 
   @Override
   public String getName() throws RemoteException {
-    // TODO Auto-generated method stub
     return null;
   }
 
